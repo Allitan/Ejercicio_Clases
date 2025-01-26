d91f539db72f8f84c89b7f9cc68a4fcfaa57dd98
Allitan <allanmendoza036@gmail.com>
2025-01-26 01:26:36 -0600

    Adding data

--- Ejercicio_Clases/Program.cs
+++ Ejercicio_Clases/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using Ejercicio_Clases;
+
+SuperHeroe superHeroe = new SuperHeroe();
+
+superHeroe.Nombre = "SuperMan";
+superHeroe.IdentidadSecreta = "Clark Kent";
+superHeroe.Ciudad = "Metropolis";
+superHeroe.PuedeVolar = true;
+superHeroe.SuperPode = "Super Fuerza";
+superHeroe.Descripcion = "Capacidad de levantar objetos muy pesados";
+superHeroe.Nivel = 10;
+superHeroe.Imprimir();
+
+SuperHeroe superHeroe2 = new SuperHeroe();
+
+superHeroe2.Nombre = "Flash";
+superHeroe2.IdentidadSecreta = "Barry Allen";
+superHeroe2.Ciudad = "Central City";
+superHeroe2.PuedeVolar = false;
+superHeroe2.SuperPode = "Velocidad Supersónica";
+superHeroe2.Descripcion = "Corre más rápido que el sonido";
+superHeroe2.Nivel = 9;
+superHeroe2.Imprimir();
+
+SuperHeroe superHeroe3 = new SuperHeroe();
+
+superHeroe3.Nombre = "Tormenta Oscura";
+superHeroe3.IdentidadSecreta = "Sofia Hernandez";
+superHeroe3.Ciudad = "Villa Tempestad";
+superHeroe3.PuedeVolar = false;
+superHeroe3.SuperPode = "Control del clima";
+superHeroe3.Descripcion = "Puede generar tormentas, relampagos y lluvia a voluntad";
+superHeroe3.Nivel = 8;
+superHeroe3.Imprimir();
+
---
+++ Ejercicio_Clases/SuperHeroe.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Ejercicio_Clases
+{
+    public class SuperHeroe : SuperPoder
+    {
+        public string? Nombre {  get; set; }
+        public string? IdentidadSecreta { get; set; }
+        public string? Ciudad {  get; set; }
+        public bool PuedeVolar {  get; set; }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede Volar: {(PuedeVolar ? "Si" : "No")}");
+            Console.WriteLine($"Super Poder: {SuperPode} ");
+            Console.WriteLine($"Descripcion: {Descripcion}");
+            Console.WriteLine($"Nivel: {Nivel} ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
